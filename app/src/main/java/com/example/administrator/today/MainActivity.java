9bbf942a4aadc9d94e2ddfabf8683ead853d1577
cty15052065642 <cty.asy@hotmail.com>
2018-05-30 18:30:32 +0800

    我修改了

--- app/src/main/java/com/example/administrator/today/MainActivity.java
+++ app/src/main/java/com/example/administrator/today/MainActivity.java
@@ -9,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //我修改了一段代码
-        //我是吕姜勇，我又来修改了一行代码
-        //哈哈，我看见了。
-        //嘿嘿
-        //蔡添宇是
-        //xxxx
     }
 }
